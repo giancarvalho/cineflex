7b7e54317a74d2bd124225a1d1cebbad5a6804d1
giancarvalho <giandcarvalho@gmail.com>
2021-09-01 14:43:56 -0300

    session is now being rendered dynamically

--- src/App.js
+++ src/App.js
@@ -2,14 +2,14 @@ import "./css/reset.css";
 import "./css/styles.css";
 import Header from "./components/header/header";
 import SelectMovie from "./components/SelectMovie";
-import Session from "./components/Session";
+import Sessions from "./components/Session";
 function App() {
   return (
     <div className="App">
       <Header />
       <main>
         {/* <SelectMovie /> */}
-        <Session />
+        <Sessions />
       </main>
     </div>
   );
